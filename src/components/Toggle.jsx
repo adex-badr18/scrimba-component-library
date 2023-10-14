2abe349b049cd6d3996b1bf594d214caffc48f15
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-14 19:25:21 +0100

    Create ToggleContext in Toggle component

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { createContext, useState } from "react";
+
+const ToggleContext = createContext();
 
 export default function Toggle({ children }) {
     const [on, setOn] = useState(false);
@@ -7,5 +9,9 @@ export default function Toggle({ children }) {
         setOn(currentOn => !currentOn);
     }
 
-    return children;
+    return (
+        <ToggleContext.Provider value={{on, toggle}}>
+            {children}
+        </ToggleContext.Provider>
+    );
 }
