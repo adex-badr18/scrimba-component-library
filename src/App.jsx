18d4cd70441ec74c7097c2e8cc28677e79e87a8a
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-14 10:37:34 +0100

    Style variants of button

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
     return (
         <main className='container'>
-            <Button size='lg' onClick={click}>Menu</Button>
+            <Button size='lg' variant='danger' onClick={click}>Menu</Button>
             {/* <Button onClick={click}>Menu</Button>
             <Button size='lg' onClick={click}>Menu</Button>
             <Button variant='success' onClick={click}>Menu</Button>
