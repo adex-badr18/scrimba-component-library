bd0444f9b4790f338c219369afbaa2d4023772fb
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-14 10:02:44 +0100

    Render buttons with different sizes and variants

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react';
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
+    function click(e) {
+        console.log(e.target);
+    }
+
     return (
         <>
-
+            <Button size='sm' onClick={click}>Menu</Button>
+            <Button onClick={click}>Menu</Button>
+            <Button size='lg' onClick={click}>Menu</Button>
+            <Button variant='success' onClick={click}>Menu</Button>
+            <Button variant='warning' onClick={click}>Menu</Button>
+            <Button variant='danger' onClick={click}>Menu</Button>
         </>
     )
 }
