aee0d9b85e8a0ee19d2a22d37450121e65b09ab5
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-14 11:37:54 +0100

    Create open staate & function that toggles the state

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
+
 export default function Menu({ children }) {
+    const [open, setOpen] = useState(false);
+
+    function toggle() {
+        setOpen(current => !current);
+    }
+
     return (
         <div className="menu">
             { children }
