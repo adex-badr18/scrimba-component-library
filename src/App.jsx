c7826d7e52b7c40284fefd7564cb834ff8f5aa11
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-14 11:09:55 +0100

    Create & render MenuDropdown/MenuItem components

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from 'react';
 import './App.css';
 import Menu from './components/Menu/Menu';
 import MenuButton from './components/Menu/MenuButton';
+import MenuDropdown from './components/Menu/MenuDropdown';
+import MenuItem from './components/Menu/MenuItem';
 
 function App() {
-    function click(e) {
-        console.log(e.target);
-    }
+    const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"];
 
     return (
         <main className='container'>
             <Menu>
                 <MenuButton>Sports</MenuButton>
+
+                <MenuDropdown>
+                    {sports.map(sport => {
+                        return <MenuItem key={sport}>{sport}</MenuItem>
+                    })}
+                </MenuDropdown>
             </Menu>
         </main>
     )
