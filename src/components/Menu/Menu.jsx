ce3351211d28ea57d527d8c5b10d44f41030f205
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-17 10:03:11 +0100

    Invoke useToggle custom hook in Menu component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,6 +2,8 @@ import React, { createContext, useState } from "react";
 import useToggle from "../../hooks/useToggle";
 
 export default function Menu({ children, onOpen }) {
+    const [open, toggleOpen] = useToggle();
+    
     return (
         <div className="menu">
             {children}
